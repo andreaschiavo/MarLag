8d3476f1d53fd5abcbb7d8e0780682d4c2e4f0b5
Andrea Schiavo <andrea.schiavo94@gmail.com>
2026-06-04 16:23:03 +0200

    options file shape updated

--- lagrangian_launcher.py
+++ lagrangian_launcher.py
@@ -3,7 +3,7 @@ from lagrlib.lagrangian import Lagrangian
 
 
 data = {
-    "grid" : "data/oceanic.csv",
+    "spawning grid" : "data/sim_grid20800.csv", 
     "grid margins" : {
         "left":12.,
         "right":23.5,
@@ -11,16 +11,15 @@ data = {
         "bottom":34.9,
     },  
     "depth margins" : (0,300),
-    "field path" :  "data/currents/",# "/data01/mediterraneo/",    "/home/schiavo/storage/CMCC_2010_2020_45/", 
-    "check field path" : "/data01/mediterraneo/2020/05/", # "/home/schiavo/storage/CMCC_2010_2020_45/"
-    "PLD": 54,
-    "PLD var": 5,
     "depth range min": 20,
     "depth range max": 250,
-    "spawning grid" : "data/sim_grid20800.csv",
     "total particles" : 1000000,
+    "PLD": 54,
+    "PLD var": 5,
     "spawning season median" : {"17": [[1, 15], [6, 9]], "18": [[12, 22], [8, 6]]}, # original [[2,5], [6,30]] - [[1,12], [8,27]]
     "spawning season duration" : {"17": [90, 105], "18": [67, 67]},
+    "field path" :  "data/currents/",
+    "check field path" : "/data01/mediterraneo/2020/05/",
     "spawning export path": "results/larvae_gen/",
     "spawning points path": "results/larvae_gen/LG_2026-06-04_FW/",
     "lagrangian export path": "results/lagrangian/",
